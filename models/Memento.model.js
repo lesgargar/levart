f95b3e7fb245dd9775274762f60d646714da6f4b
Leslie <leslie@iMac-de-Leslie.local>
2022-11-20 11:14:37 -0600

    git created time to work :tada:

---
+++ models/Memento.model.js
@@ -0,0 +1,14 @@
+const {Schema, model} = require("mongoose");
+
+const mementoSchema = new Schema(
+    {
+    name:String,
+    description:{
+        type:String,
+        required: true ,
+        minlength: 20
+        },
+    }
+);
+
+module.exports = model("Memento", mementoSchema);
