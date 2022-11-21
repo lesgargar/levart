a72eb03665a8f1f9447646a1412d82aaf5b3db17
Leslie <leslie@iMac-de-Leslie.local>
2022-11-21 12:00:45 -0600

    test memento

--- app.js
+++ app.js
@@ -32,6 +32,9 @@ app.use("/", indexRoutes);
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
+const mementoRoutes = require("./routes/memento.routes")
+app.use("/memento", mementoRoutes)
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
