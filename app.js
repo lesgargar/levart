b5b22d81f0d5a5dd5bfdf4deacbad13720e90f3f
Alexis Tellez <alexiztg@gmail.com>
2022-11-21 19:21:38 -0600

    CRUD user and half crud review

--- app.js
+++ app.js
@@ -38,6 +38,8 @@ app.use("/memento", mementoRoutes)
 const reviewRoutes = require("./routes/review.routes")
 app.use("/review", reviewRoutes)
 
+
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
