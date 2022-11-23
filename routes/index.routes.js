816609cbe5bdfbb786cde8b7617e3b19bd6451cb
Leslie <leslie@iMac-de-Leslie.local>
2022-11-23 12:24:08 -0600

    profile views with cities done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const router = express.Router();
-
+const Memento = require("../models/Memento.model");
+const City = require("../models/City.model");
 /* GET home page */
-router.get("/",  (req, res, next) => {
-  if ( !{currentUser:req.session.currentUser}){
-    res.render("users/user-detail", {currentUser:req.session.currentUser});
-  }else{
+router.get("/",  async(req, res, next) => {
+  try{
+   
+    const {_id} = req.session.currentUser
+    const cities = await City.find({owner:_id})
+   
+    if ( req.session.currentUser){
+      res.render("users/user-detail", {cities, currentUser:req.session.currentUser  })
+    }else{
+      res.render("home")
+    }
+   }catch(err){
     res.render("home")
-  }
-  // try{ 
-  //   const currentUser = await req.session.currentUser
-  // res.render("users/user-detail");
-  // }catch{
-  //   res.render("/home")
-  // }
+}
+
 });
 
 
